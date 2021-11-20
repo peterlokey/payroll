ee1311f2ba374a8e83b740f0d49609337fed788b
peterlokey <peter.lokey1@gmail.com>
2021-11-19 20:22:37 -0600

    Local database creation

---
+++ src/test/java/com/lokey/payroll/PayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lokey.payroll;
+
+import org.testng.annotations.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
